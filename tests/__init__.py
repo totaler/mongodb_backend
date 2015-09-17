dc2b159ddc3b2fbed81d5a57a5e40a7d7d66b6c1
Eduard Carreras <ecarreras@gisce.net>
2015-09-17 11:51:02 +0200

    Add tests for it

---
+++ tests/__init__.py
@@ -0,0 +1,33 @@
+import unittest
+
+from destral import testing
+from expects import *
+
+
+class MongoDBBackendTest(testing.OOTestCase):
+
+    @unittest.skip('No views defined in this module')
+    def test_all_views(self):
+        pass
+
+    def test_mdbpool(self):
+        from mongodb_backend.mongodb2 import mdbpool
+        expect(mdbpool._connection).to(be_none)
+
+        # If we try to access to the connection mongodb connects
+        expect(mdbpool.connection).to_not(be_none)
+
+    def test_default_mongodb_name(self):
+        from mongodb_backend.mongodb2 import mdbpool
+        expect(self.openerp.config.options).to_not(have_keys(
+            'mongodb_name',
+            'mongodb_port',
+            'mongodb_host',
+            'mongodb_user',
+            'mongodb_pass'
+        ))
+        # After accessing to getting object variables are defined
+        db = mdbpool.get_db()
+        expect(self.openerp.config.options).to(have_keys(
+            mongodb_name=self.database
+        ))
