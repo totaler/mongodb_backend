92ff44d5ba2db3e1a6e3e12b7f30630ee6bcdcd1
Eduard Carreras <ecarreras@gisce.net>
2019-10-31 17:28:35 +0100

    Bump to v0.5.3

--- __terp__.py
+++ __terp__.py
@@ -5,7 +5,7 @@ NoSQL MongoDB backend for OpenERP. It allows to create objects and store
 all the data in a NoSQL way. You can interact with this data the same way
 as it was stored in PostgreSQL from the client.
     """,
-  "version": "0.5.2",
+  "version": "0.5.3",
   "author": "Joan M. Grande",
   "license" : "GPL-3",
   "category": "Generic Modules",
